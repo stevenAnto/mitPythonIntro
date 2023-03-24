7804ec90d031fbc2c8284a95ecaed6409b4a2b6e
stevenAnto <ecalcinap@unsa.edu.pe>
2023-03-24 09:38:48 -0500

    palindromo recursivo

---
+++ IntroComputSciencProPy/palindrome.py
@@ -0,0 +1,20 @@
+def isPalindrome(s):
+    """Assume s is a str
+    return True if letter in s form a palindrome
+    ;False otherwise. Non-letters and capitalization are ignored"""
+    def toChars(s):
+        s = s.lower()
+        letters = ''
+        for c in s:
+            if c  in 'abcdefghijklmopqrstuvwxyz':
+                letters = letters+c
+        return letters
+    def isPal(s):
+        if len(s)<=1:
+            return True
+        else:
+            return s[0]==s[-1] and isPal(s[1:-1])
+    return isPal(toChars(s))
+
+string = input('ingrese texto a evaluar')
+print(isPalindrome(string))
