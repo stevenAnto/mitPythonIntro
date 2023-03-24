7cf1550a587c1c7fb0f95650dd204b7c6d64b0d1
stevenAnto <ecalcinap@unsa.edu.pe>
2023-03-24 10:32:27 -0500

    alternative form to import modules

--- IntroComputSciencProPy/modules.py
+++ IntroComputSciencProPy/modules.py
@@ -1,9 +1,14 @@
-import circle
+#import circle
+#Esta forma permite accder a los objetos del modulo sin nombrar el nombre del modulo.
+from circle import *
+print(pi)
+print(circle.pi)
 
-pi=3
+"""pi=3
 print(pi)
 print(circle.pi)
 print(circle.area(3))
 print(circle.circumference(3))
 print(circle.sphereSurface(3))
 #print(circle.sphereVolum(3))
+"""
