7e99b811575f32e9300b5fbf658bd9669e5ff10a
stevenAnto <ecalcinap@unsa.edu.pe>
2023-03-25 10:50:25 -0500

    me olvide

---
+++ IntroComputSciencProPy/otherEx.py
@@ -0,0 +1,6 @@
+birthStones = {'Jan':'Garnet', 'Feb':'Amethyst', 'Mar':'Acquamarine','Apr':'Diamond', 'May':'Emerald'}
+
+months =birthStones.keys()
+print(months)
+birthStones['June']='Pearl'
+print(months)
