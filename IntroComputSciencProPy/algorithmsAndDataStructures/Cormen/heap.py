14f11a6b8f78db755215c6e137fe45df68869fd0
stevenAnto <ecalcinap@unsa.edu.pe>
2023-04-02 13:50:27 -0500

    first implementatio of heap

---
+++ IntroComputSciencProPy/algorithmsAndDataStructures/Cormen/heap.py
@@ -0,0 +1,45 @@
+#Remember the heap is storaged in an array,
+#Where parent(i)=celing (i/2)
+#leftChild(i)=2*i
+#rightChild(i)=2*i+1
+
+
+def leftChild(i):
+    return 2*i
+
+
+def rightChild(i):
+    return 2*i+1
+
+def maxHeapify(vector,i):
+    print("entroindice",i)
+    l=leftChild(i)
+    r=rightChild(i)
+    print("l,r",l," ",r)
+    if l<len(vector) and vector[l]>vector[i]:
+        largest =l
+    else:
+        largest =i
+    if r<len(vector) and vector[r]>vector[largest]:
+        largest =r
+    if largest != i:
+        #excechange
+        aux = vector[i]
+        vector[i]=vector[largest]
+        vector[largest]=aux
+        maxHeapify(vector,largest)
+
+
+def buildMaxHeap(vector):
+    size =len(vector)
+    for num in reversed(range(len(vector))):
+        maxHeapify(vector,num)
+        print("ordenado:",a)
+
+#prueba
+
+a = [4,1,3,2,16,9,10,14,8,7]
+print(a)
+buildMaxHeap(a)
+print(a)
+
