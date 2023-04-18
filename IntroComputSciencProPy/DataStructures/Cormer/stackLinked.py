567664ac9d445367acbe4ce5d3aa891f18a52ef7
stevenAnto <ecalcinap@unsa.edu.pe>
2023-04-18 13:56:09 -0500

    stack with LinkedList;'

---
+++ IntroComputSciencProPy/DataStructures/Cormer/stackLinked.py
@@ -0,0 +1,18 @@
+import linkedList
+
+class stackLinked(object):
+    def __init__(self):
+        self.linkedList = linkedList.LinkedList()
+        self.top = self.linkedList.head
+    def pushL(self,value):
+        nodo = linkedList.Node(value)
+        nodo.next =self.top
+        self.top = nodo
+    def pop(self):
+        if self.top==None:
+            raise ValueError('underflow')
+        else:
+            nodo = linkedList.top
+            self.top = LinkedList.top.next
+            return nodo
+
